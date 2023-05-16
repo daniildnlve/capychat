5758e998e8d7c2e5f799798dd5f9ab08e7419a38
Daniil Vonyavkin <daniildnlve@gmail.com>
2023-05-17 04:31:00 +0500

    create app and add basic components and libraries

---
+++ src/components/App/App.jsx
@@ -0,0 +1,17 @@
+import { Routes, Route } from 'react-router-dom';
+
+import { PATH_HOMEPAGE } from '../../paths/path';
+
+const App = () => {
+  return (
+    <>
+      <Routes>
+        <Route path={PATH_HOMEPAGE}>
+
+        </Route>
+      </Routes>
+    </>
+  )
+}
+
+export default App;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,19 @@
+const Header = () => {
+  return (
+    <header>
+      <ul>
+        <li>
+          <button></button> 
+        </li>
+        <li>
+          <p></p>
+        </li>
+        <li>
+          <button></button>
+        </li>
+      </ul>
+    </header>
+  )
+}
+
+export default Header;
